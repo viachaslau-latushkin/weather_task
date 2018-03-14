dcae8832327f0c93ca06240ad7e3ebdf2e5dd588
viachaslau-latushkin <viachaslau.latushkin@gmail.com>
2018-03-14 10:07:56 +0300

    new path for gh-pages

--- js/directive/result.directive.js
+++ js/directive/result.directive.js
@@ -2,7 +2,7 @@ angular.module('weatherApp').
   directive('mainResult',function(){
     return {
       restrict:"A",
-      templateUrl: "/html/result.html",
+      templateUrl: "../../html/result.html",
       link: function(scope,element,attr){
         scope.delete = function(city){
           scope.availableCity[city].enable = true;
